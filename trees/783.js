c15f8c69ce067a9e8b1d87b7f7841353b3f61915
Ulyana Kiklevich <ulya.kiklevich@gmail.com>
2023-02-18 16:42:16 +0100

    solutions day 21

--- trees/783.js
+++ trees/783.js
@@ -8,27 +8,61 @@ function TreeNode(val, left, right) {
  * @param {TreeNode} root
  * @return {number}
  */
-const minDiffInBST = function(root) {
+const minDiffInBSTMy = function(root) {
   const diff = []
   const t = (root) => {
     if (root) {
-      if (root.left?.val) {
-        diff.push(Math.abs(root.val - root.left?.val))
-      }
-      if (root.right?.val) {
-        diff.push(Math.abs(root.val - root.right?.val))
-      }
+
       t(root.left)
+      console.log(root.val)
+      if (root.left) {
+        diff.push(Math.abs(root.val - root.left.val))
+      }
+      if (root.right) {
+        diff.push(Math.abs(root.val - root.right.val))
+      }
+
       t(root.right)
     }
   }
 
   t(root)
 
+  console.log(diff)
+
   return Math.min(...diff)
+}
+/*
+  **
+  * @param {TreeNode} root
+  * @return {number}
+  */
+const minDiffInBST = function(root) {
+  let res = 100000, pre = -1
+
+  const t = (root) => {
+    if (root.left) {
+      t(root.left)
+    }
+    if (pre >= 0) {
+      res = Math.min(res, root.val - pre)
+    }
+
+    pre = root.val
+
+    if (root.right) {
+      t(root.right)
+    }
+  }
+
+  t(root)
+
+  return res
 }
 
 /*const root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))*/
 const root = new TreeNode(4, new TreeNode(6), new TreeNode(2, new TreeNode(1), new TreeNode(3)))
 
-console.log(minDiffInBST(root))
+const root1 = new TreeNode(90, new TreeNode(69, new TreeNode(49, undefined, new TreeNode(52)), new TreeNode(89)) )
+
+console.log(minDiffInBST(root1))
